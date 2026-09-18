1fd39fdd105419ec6d22290cfe681182d40ec23f
stanleywoosweeleong <130345527+stanleywoosweeleong@users.noreply.github.com>
2026-09-19 06:07:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'formulab-v62-20260917';
+const CACHE = 'formulab-v65-20260917';
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png']))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
